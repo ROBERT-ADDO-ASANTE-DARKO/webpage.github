9d9350aab69fc4caf12e3fd390889eaf6d7be57a
ROBERT-ADDO-ASANTE-DARKO <77078438+ROBERT-ADDO-ASANTE-DARKO@users.noreply.github.com>
2021-01-19 06:37:57 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+/*function initMap(){
+    var location = {lat: 5.572790, lng: -0.209330};
+    var map = new google.maps.Map(document.querySelector("#map"),
+      {
+      zoom: 4,
+      center: location
+    });
+    var marker = new google.maps.Marker({
+      position: location,
+      map: map
+    });
+  }*/
